f1e507987577c252be1963021a001a4f079643ce
Soumyabrata Sen <soumyabratasen@hotmail.com>
2023-02-18 19:10:45 +0530

    Initial Commit

---
+++ accounts/src/main/java/com/sam09/ms/a2z/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package com.sam09.ms.a2z.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ accounts/src/test/java/com/sam09/ms/a2z/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam09.ms.a2z.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
